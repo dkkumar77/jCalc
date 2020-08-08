30f32503ec572ba86f7b83bc8413da7acccadef4
dkkumar77 <bcs.deepakkumar@gmail.com>
2020-08-08 00:12:57 -0400

    added one method

--- src/Calc/Controllers/Controller.java
+++ src/Calc/Controllers/Controller.java
@@ -13,7 +13,9 @@ public class Controller {
     private ArrayList<String> operands = new ArrayList<>();
 
     private boolean lastOperand = false;
-    private boolean oneDecimal = false;
+
+
+    //private boolean oneDecimal = false;
 
 
     private String currentString = "";
@@ -56,63 +58,100 @@ public class Controller {
     void handleAdd(ActionEvent event) {
 
         if(event.getSource().equals(add)){
-            integer.add(currentString);
-            operands.add("+");
-            currentString = "";
-            lastOperand = true;
+            if(!currentString.isEmpty()) {
+                integer.add(currentString);
+                operands.add("+");
+                currentString = "";
+                lastOperand = true;
+
+
+            }
+
 
         }
     }
 
     @FXML
     void handleClear(ActionEvent event) {
-        integer.clear();
-        operands.clear();
-        lastOperand = false;
-        oneDecimal = false;
-        currentString = "";
-        output.setText("");
+        if(event.getSource().equals(clear)) {
+            integer.clear();
+            operands.clear();
+            lastOperand = false;
+            currentString = "";
+            output.setText("");
+        }
+
 
     }
 
     @FXML
     void handleDivision(ActionEvent event) {
 
+        if(event.getSource().equals(division)){
+
+        }
     }
 
     @FXML
     void handleEqual(ActionEvent event) {
+        if(event.getSource().equals(equal)){
+            integer.add(currentString);
+            Integer solution = Integer.parseInt(integer.get(0));
+
+            if(integer.size()% 2 == 0){
+                for(int i = 1; i < integer.size(); i++){
+                    for(int g = 0; g< operands.size(); g++){
+                        if(operands.get(g).equals("+")){
+                            solution = solution + Integer.parseInt(integer.get(i));
+                        }
+                    }
+                }
+                output.setText(solution.toString());
+
+            }
+
+        }
 
     }
 
     @FXML
     void handleModulus(ActionEvent event) {
+        if(event.getSource().equals(modulus)){
+
+        }
 
     }
 
     @FXML
     void handleMultiplication(ActionEvent event) {
+        if(event.getSource().equals(multiply)){
+
+        }
 
     }
 
     @FXML
     void handleNegate(ActionEvent event) {
+        if(event.getSource().equals(negate)){
+
+
+        }
 
     }
 
     @FXML
     void handleNumbers(ActionEvent event) {
 
-        if(event.getSource().equals(one)){ currentString = currentString + Numbers.ONE; }
+        if(event.getSource().equals(one)){ currentString = currentString + Numbers.ONE;}
         if(event.getSource().equals(two)){ currentString = currentString + Numbers.TWO;}
-        if(event.getSource().equals(three)){ currentString = currentString + (Numbers.THREE);}
-        if(event.getSource().equals(four)){currentString = currentString + (Numbers.FOUR);}
-        if(event.getSource().equals(five)){ currentString = currentString + (Numbers.FIVE);}
+        if(event.getSource().equals(three)){ currentString = currentString + (Numbers.THREE); }
+        if(event.getSource().equals(four)){currentString = currentString + (Numbers.FOUR); }
+        if(event.getSource().equals(five)){ currentString = currentString + (Numbers.FIVE); }
         if(event.getSource().equals(six)){ currentString = currentString + (Numbers.SIX);}
-        if(event.getSource().equals(seven)){ currentString = currentString + (Numbers.SEVEN);}
-        if(event.getSource().equals(eight)){ currentString = currentString + (Numbers.EIGHT);}
-        if(event.getSource().equals(nine)){ currentString = currentString + (Numbers.NINE);}
-        if(event.getSource().equals(zero)){ currentString = currentString + (Numbers.ZERO);}
+        if(event.getSource().equals(seven)){ currentString = currentString + (Numbers.SEVEN); }
+        if(event.getSource().equals(eight)){ currentString = currentString + (Numbers.EIGHT); }
+        if(event.getSource().equals(nine)){ currentString = currentString + (Numbers.NINE); }
+        if(event.getSource().equals(zero)){ currentString = currentString + (Numbers.ZERO); }
 
         output.setText(currentString);
 
@@ -129,7 +168,9 @@ public class Controller {
 
     @FXML
     void handleSubtract(ActionEvent event) {
+        if(event.getSource().equals(subtract)){
 
+        }
     }
 
 }
